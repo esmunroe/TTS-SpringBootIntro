3fb881fad2e80458ab3ce8d4d695860f05f17874
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-22 01:19:41 -0500

    Created object class model

--- src/main/java/com/tts/springbootintro/models/Intro.java
+++ src/main/java/com/tts/springbootintro/models/Intro.java
@@ -1,5 +1,69 @@
 package com.tts.springbootintro.models;
 
-public class Intro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Intro {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String firstName;
+	private String lastName;
+	private int age;
+	private int gradeLevel;
+	
+	public Intro() {}
+
+	public Intro(String firstName, String lastName, int age, int gradeLevel) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.gradeLevel = gradeLevel;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getGradeLevel() {
+		return gradeLevel;
+	}
+
+	public void setGradeLevel(int gradeLevel) {
+		this.gradeLevel = gradeLevel;
+	}
+
+	@Override
+	public String toString() {
+		return "Intro [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age
+				+ ", gradeLevel=" + gradeLevel + "]";
+	}
 }
