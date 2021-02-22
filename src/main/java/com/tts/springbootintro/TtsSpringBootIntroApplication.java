10b5acc16fdd14922929e895e8f3458e432e99b7
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-21 21:54:21 -0500

    Initial commit

---
+++ src/main/java/com/tts/springbootintro/TtsSpringBootIntroApplication.java
@@ -0,0 +1,13 @@
+package com.tts.springbootintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsSpringBootIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsSpringBootIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/springbootintro/TtsSpringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsSpringBootIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
